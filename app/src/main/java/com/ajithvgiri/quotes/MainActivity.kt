f181dec347d61cc76676fafeab6610c9be1844f6
ajithvgiri <ajithvgiri@qkopy.com>
2019-09-10 22:57:05 +0530

    migrated to Kotlin

---
+++ app/src/main/java/com/ajithvgiri/quotes/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ajithvgiri.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
